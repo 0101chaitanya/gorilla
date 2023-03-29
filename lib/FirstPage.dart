18631625abd853ee2a829f992c6a3d82ebb53e80
chaitanya <0101chaitanya@gmail.com>
2023-03-29 11:42:05 +0530

    added will pop scope

--- lib/FirstPage.dart
+++ lib/FirstPage.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'dart:developer' as dev;
 
 class FirstPage extends StatefulWidget {
   const FirstPage({Key? key}) : super(key: key);
@@ -14,30 +15,42 @@ class _FirstPageState extends State<FirstPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text("First Page"),
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            const Text(
-              "First Page",
-              style: TextStyle(fontSize: 36),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  number = Random().nextInt(100);
-                  Navigator.pop(context, number);
-                  // Navigator.of(context).pop(number);
-                },
-                child: const Text(
-                  "Back button",
-                  style: TextStyle(fontSize: 36),
-                ))
-          ],
+    return WillPopScope(
+      onWillPop: () {
+        //controls what happend when back button is pressed
+        dev.log("willpopscope is run");
+        number = Random().nextInt(100);
+        Navigator.pop(context, number);
+        //if true then screen is allowed to pop (going back)
+        // else screen is not popped
+        return Future.value(false);
+        // return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text("First Page"),
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Text(
+                "First Page",
+                style: TextStyle(fontSize: 36),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    number = Random().nextInt(100);
+                    Navigator.pop(context, number);
+                    // Navigator.of(context).pop(number);
+                  },
+                  child: const Text(
+                    "Back button",
+                    style: TextStyle(fontSize: 36),
+                  ))
+            ],
+          ),
         ),
       ),
     );
